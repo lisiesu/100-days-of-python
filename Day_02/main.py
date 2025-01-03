66e79a40f5123a113ce9f1dff973407fe7889cb6
Aline Silva <lisie_su@yahoo.com.br>
2025-01-03 15:01:36 +0000

    Add different solution

--- Day_02/main.py
+++ Day_02/main.py
@@ -5,6 +5,15 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+# Different solution
+# tip_as_percentage =  tip / 100
+# total_tip_amount = bill * tip_as_percentage
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# total = round(bill_per_person, 2)
+# print(f"Each person should pay £{total}")
+
+# My solution
 tip_percentage =  tip / 100
 individual_total = bill / people
 individual_tip = individual_total * tip_percentage
