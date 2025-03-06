cd046829d93e0bb090b703ee8ac2cd76c48acb63
Aline Silva <lisie_su@yahoo.com.br>
2025-03-06 22:19:17 +0000

    Nested list in Dictionary

--- Day_09/main.py
+++ Day_09/main.py
@@ -1,27 +1,31 @@
-# Creating a dictionary
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.",
-    "Function": "A piece of code that you can easily call over and over again.",
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
 }
 
-# Retrieving a value from a dictionary
-print(programming_dictionary["Function"])
+# Nested List in Dictionary
 
-# Adding more items to a dictionary
-programming_dictionary["Loop"] = "The action of doing something over and over again."
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"],
+#     "Germany": ["Stuttgart", "Berlin"],
+# }
 
-# Creating an empty dictionary
-empty_dictionary = {}
+# print Lille
+# print(travel_log["France"][1])
 
-# Wipe an existing dictionary
-# programming_dictionary = {}
-# print(programming_dictionary)
+nested_list = ["A", "B", ["C", "D"]]
+# print(nested_list[2][1])
 
-# Edit an item in a dictionary
-programming_dictionary["Bug"] = "A moth in your computer."
-# print(programming_dictionary)
+# Nested dictionary in a dictionary
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
 
-# Loop through a dictionary
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+print(travel_log["Germany"]["cities_visited"][2])
