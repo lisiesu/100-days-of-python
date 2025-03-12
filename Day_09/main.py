a68a065dc6bcd6871d0a9d4b1ccaeec40d34de04
Aline Silva <lisie_su@yahoo.com.br>
2025-03-12 18:55:48 +0000

    Auction exercise

--- Day_09/main.py
+++ Day_09/main.py
@@ -1,31 +1,27 @@
-capitals = {
-    "France": "Paris",
-    "Germany": "Berlin",
-}
+from art import logo
+print(logo)
 
-# Nested List in Dictionary
 
-# travel_log = {
-#     "France": ["Paris", "Lille", "Dijon"],
-#     "Germany": ["Stuttgart", "Berlin"],
-# }
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-# print Lille
-# print(travel_log["France"][1])
 
-nested_list = ["A", "B", ["C", "D"]]
-# print(nested_list[2][1])
-
-# Nested dictionary in a dictionary
-travel_log = {
-  "France": {
-    "cities_visited": ["Paris", "Lille", "Dijon"],
-    "total_visits": 12
-   },
-  "Germany": {
-    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-    "total_visits": 5
-   },
-}
-
-print(travel_log["Germany"]["cities_visited"][2])
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
